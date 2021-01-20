9f53ac27ad79512d295d9a5a3cc77f36b9bb5da4
Joe Farrington <farrington.joe@gmail.com>
2021-01-20 14:56:19 +0000

    Correct range of hyperparameters

--- wine_quality/sklearn_optuna.py
+++ wine_quality/sklearn_optuna.py
@@ -47,8 +47,8 @@ def load_data(path, label_col):
 
 
 def suggest_hyperparameters(trial):
-    alpha = trial.suggest_float("alpha", 1e-4, 10, log=True)
-    l1_ratio = trial.suggest_float("l1_ratio", 1e-4, 10, log=True)
+    alpha = trial.suggest_float("alpha", 1e-4, 1, log=True)
+    l1_ratio = trial.suggest_float("l1_ratio", 1e-4, 1, log=True)
     return alpha, l1_ratio
 
 
